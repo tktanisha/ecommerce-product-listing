f7fcfe6ea268977f12f8191915b36ab614cd0267
Tanisha KhandelwaL <khandelwaltanisha04@gmail.com>
2025-10-29 21:44:51 +0530

    minor changes

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductService } from '../../services/product.service';
-import { ProductCardComponent } from '../../component/product-card/product-card.component';
-import { Product } from '../../models/product.model';
-import { ProductFilters } from '../../models/productFilter.model';
-import { ProductShimmerComponent } from '../../component/product-shimmer/product-shimmer.component';
-import { SortingFilterComponent } from '../../component/product-filter/sorting-filter/sorting-filter.component';
-import { PriceSliderComponent } from '../../component/product-filter/price-slider/price-slider.component';
+
+import { ButtonModule } from 'primeng/button';
+
 import { CategoryFilterComponent } from '../../component/product-filter/category-filter/category-filter.component';
 import { PaginationComponent } from '../../component/pagination/pagination.component';
+import { PriceSliderComponent } from '../../component/product-filter/price-slider/price-slider.component';
+import { ProductCardComponent } from '../../component/product-card/product-card.component';
+import { ProductShimmerComponent } from '../../component/product-shimmer/product-shimmer.component';
+import { SortingFilterComponent } from '../../component/product-filter/sorting-filter/sorting-filter.component';
+
+import { Product } from '../../models/product.model';
+import { ProductFilters } from '../../models/productFilter.model';
 import { ProductsResponse } from '../../models/products-response.model';
-import { ButtonModule } from 'primeng/button';
-import { ActivatedRoute, Router } from '@angular/router';
+
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -30,16 +34,23 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-  products: Product[] = [];
-  allProducts: Product[] = [];
-  loading = true;
+  productService: ProductService = inject(ProductService);
+  route: ActivatedRoute = inject(ActivatedRoute);
+  router: Router = inject(Router);
+
   error = '';
+
   totalProducts = 0;
   minPrice = 0;
   maxPrice = 0;
+
+  clearButtonDisabled = true;
+  loading = true;
   selectedCategory: string | null = null;
   resetFilters = false;
-  clearButtonDisabled = true;
+
+  products: Product[] = [];
+  allProducts: Product[] = [];
 
   filters: ProductFilters = {
     limit: 9,
@@ -48,53 +59,48 @@ export class ProductsComponent implements OnInit {
     order: null,
   };
 
-  constructor(
-    private productService: ProductService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {}
-
+  // Why it's different: The snapshot provides a static, one-time read of the route's parameters. If a user changes query parameters while staying on the same component (e.g., by clicking a filter), the component will not be notified of the change and will not update. This would break your filter functionality.
+  // When to use it: This approach is suitable for cases where you only need the initial values of the query parameters and don't expect them to change while the component is active.
   ngOnInit(): void {
-  this.route.queryParams.subscribe(params => {
-    this.filters = {
-      limit: 9,
-      skip: 0,
-      sortBy: params['sortBy'] ? 'price' : null,
-      order: params['order'] || null
-    };
+    // The code subscribes to the queryParams observable, which is a stream of values representing the query parameters in the URL (e.g., ?category=electronics&order=asc). The code inside the subscription block will execute every time the query parameters change.
+    this.route.queryParams.subscribe((params) => {
+      this.filters = {
+        limit: 9,
+        skip: 0,
+        sortBy: params['sortBy'] ? 'price' : null, //params['sortBy'] ? 'price' : null: This line checks if the sortBy query parameter exists. If it does, it explicitly sets filters.sortBy to 'price'. This is likely because the sorting logic in the onSortChange method only handles price-based sorting
+        order: params['order'] || null,
+      };
 
-    this.selectedCategory = params['category'] || null;
+      this.selectedCategory = params['category'] || null;
 
-    this.loadProducts();
-  });
+      this.loadProducts();
+    });
   }
 
-private updateQueryParams(): void {
-  const params: any = {};
+  private updateQueryParams(): void {
+    const params: any = {};
 
-  if (this.filters.sortBy) params.sortBy = this.filters.sortBy;
-  if (this.filters.order) params.order = this.filters.order;
-  if (this.selectedCategory) params.category = this.selectedCategory;
+    if (this.filters.sortBy) params.sortBy = this.filters.sortBy;
+    if (this.filters.order) params.order = this.filters.order;
+    if (this.selectedCategory) params.category = this.selectedCategory;
 
-  this.router.navigate([], {
-    relativeTo: this.route,
-    queryParams: params,
-    queryParamsHandling: 'merge'
-  });
-}
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: params,
+      queryParamsHandling: 'merge',
+    });
+  }
 
   loadProducts(): void {
     this.loading = true;
-    this.error = '';
 
-    const request$ = this.selectedCategory
+    (this.selectedCategory
       ? this.productService.getProductsByCategory(
           this.selectedCategory,
           this.filters
         )
-      : this.productService.getProducts(this.filters);
-
-    request$.subscribe({
+      : this.productService.getProducts(this.filters)
+    ).subscribe({
       next: (response: ProductsResponse) => {
         if (this.filters.skip === 0) {
           this.allProducts = [];
@@ -147,20 +153,20 @@ private updateQueryParams(): void {
     this.updateQueryParams();
   }
 
-  clearFilters(): void {
-    this.selectedCategory = null;
-    this.resetFilters = true;
+  // clearFilters(): void {
+  //   this.selectedCategory = null;
+  //   this.resetFilters = true;
 
-    this.filters = {
-      limit: 9,
-      skip: 0,
-      sortBy: null,
-      order: null,
-    };
+  //   this.filters = {
+  //     limit: 9,
+  //     skip: 0,
+  //     sortBy: null,
+  //     order: null,
+  //   };
 
-    this.clearButtonDisabled = true;
-    this.updateQueryParams();
-  }
+  //   this.clearButtonDisabled = true;
+  //   this.updateQueryParams();
+  // }
 
   private resetSkip(): void {
     this.filters.skip = 0;
